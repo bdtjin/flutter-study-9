160ef47229968afdd65b22792b7da6c421c9f644
jin <bdtjin00@gmail.com>
2025-12-08 16:41:03 +0900

    9단까지 만들어보기

--- code/d251208_2.dart
+++ code/d251208_2.dart
@@ -1,28 +1,22 @@
+void main() {
+  // 2는 고정
+  // 곱해지는 수가 1씩 증가함
 
-void main(){
-   // 2는 고정
-   // 곱해지는 수가 1씩 증가함
+  // 1번째 단계: 1부터 9까지 출력해보기
+  // 2번째 단계: 구구단 2단부터 9단까지 출력
 
-   // 1번째 단계: 1부터 9까지 출력해보기
-   // 2번째 단계: 
+  // for (var i = 1; i <= 9; i++) {
+  //   var count = 2 * i;
+  //   print('2 * $i = $count');
+  //   // print('--------------');
+  //   // var threecount = 3 * i;
+  //   // print('3 * $i = $threecount');
+  // }
 
-
-for(var i = 1; i <= 9; i++){
-  // print(i);
-  var count = 2 * i;
-  print('2 * $i = $count');
-  
-}
-
-// print('2 * 1 = 2');
-// print('2 * 2 = 4');
-// print('2 * 3 = 6');
-// print('2 * 4 = 8');
-// print('2 * 5 = 10');
-// print('2 * 6 = 12');
-// print('2 * 7 = 14');
-// print('2 * 8 = 16');
-// print('2 * 9 = 18');
-
-
+  for(var gugu = 2; gugu <= 9; gugu++){
+    for(var i = 1; i <= 9; i++){
+      var count = gugu * i;
+      print('$gugu * $i = $count');
+    }
+  }
 }
