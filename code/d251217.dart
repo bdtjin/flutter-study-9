1448a8f729e6bbc60fc9cd3eb011277e5129e45a
jin <bdtjin00@gmail.com>
2025-12-17 09:54:55 +0900

    feat: 네임드생성자

--- code/d251217.dart
+++ code/d251217.dart
@@ -1,10 +1,29 @@
 void main() {
   //
+  printABC(10, "BBB", "CCC");
+  // print("BBB", 10, "BBB"); => 순서가 바뀌면 안 됨
+
+  printDEF(d: 10, e: "EEE", f: "FFF");
+  printDEF(e: "EEE", d: 10, f: "FFF");
+}
+
+// 섞어서 사용 가능
+// 앞에는 위치 파라미터, 뒤에 네임드 파리미터 사용 가능
+void printGHI(String g, {required int h, String? i}) {
+  print("g: $g h: $h i: $i");
 }
 
 // 함수의 파라미터
 // 1. 위치 파라미터 => 소괄호 안에 바로 타입과 변수명 선언!
+// 사용할 땐 반드시 파라미터 선언된 순서대로만 사용!
 void printABC(int a, String b, String c) {
   print("a: $a b: $b, c:$c");
 }
+
 // 2. 네임드 파라미터
+// required 키워드는 함수 사용 시 반드시 넘겨줘야한다는 키워드!
+// 사용할 때 파라미터 순서 상관 X
+void printDEF({required d, required e, required f}) {
+  //
+  print("d: $d e: $e f: $f");
+}
--- code/d251217_2.dart
+++ code/d251217_2.dart
@@ -1,12 +1,15 @@
 void main() {
   //
   print("프로그램 실행됨");
-  Human h1 = Human("강성훈", age: 20, gender: "MALE", hairColor: "Black");
+  // Human h1 = Human("강성훈", age: 20, gender: "MALE", hairColor: "Black", skinColor: "WHITE");
+  Human h1 = Human.whiteSkin("강성훈", age: 20, gender: "MALE");
+
   Human h2 = Human(
     "이지원",
     age: 20,
     gender: "MALE",
-    child: Human("상구", age: 9, gender: "MALE"),
+    skinColor: "BLACK",
+    child: Human("상구", age: 9, gender: "MALE", skinColor: "YELLOW"),
   );
 }
 
@@ -21,11 +24,41 @@ class Human {
   String gender;
   String? hairColor;
   Human? child;
+  String skinColor;
+
   Human(
     this.name, {
     required this.age,
     required this.gender,
     this.hairColor,
     this.child,
+    required this.skinColor,
   });
+  // 네임드 생성자
+  // 객체를 생성하면서 특정한 값을 할당하거나 특정한 기능을 수행하게 하고 싶을 때 사용
+  // 특정한 역할을 한다는걸 객체 생성할 때 알기 쉽게 하기 위해서
+  // 생성자에 이름을 붙일 수 있음!
+  Human.blackSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.hairColor,
+    this.child,
+  }) : this.skinColor = "BLACK";
+
+  Human.whiteSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.hairColor,
+    this.child,
+  }) : this.skinColor = "WHITE";
+
+  Human.yellowSkin(
+    this.name, {
+    required this.age,
+    required this.gender,
+    this.hairColor,
+    this.child,
+  }) : this.skinColor = "YELLOW";
 }
